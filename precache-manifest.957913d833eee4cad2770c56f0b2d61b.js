db45ca4c800560c08843f8be11aa57fe2a4110b9
Zoruka <zo.fmendes@gmail.com>
2020-10-26 16:03:27 -0300

    Updates

---
+++ precache-manifest.957913d833eee4cad2770c56f0b2d61b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86f4955867c233a396a985564ddc559b",
+    "url": "/kumi/index.html"
+  },
+  {
+    "revision": "4ebfea493e0cdc59c773",
+    "url": "/kumi/static/js/2.d376e168.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/kumi/static/js/2.d376e168.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e64ee1f5a7be3b2c5f85",
+    "url": "/kumi/static/js/main.33df8ddf.chunk.js"
+  },
+  {
+    "revision": "c61741c6b94b963a74e3",
+    "url": "/kumi/static/js/runtime-main.e727a0ae.js"
+  }
+]);
